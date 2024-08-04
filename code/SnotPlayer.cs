da6fed2e4be4a367466cf879076d01a77b21d349
Marc Ortiz López <xdmarcitos@hotmail.com>
2024-08-04 17:37:53 +0200

    upd

---
+++ code/SnotPlayer.cs
@@ -0,0 +1,131 @@
+using Sandbox;
+using Sandbox.Citizen;
+
+public sealed class SnotPlayer : Component
+{
+	[Property]
+	[Category("Components")]
+	public GameObject Camera { get; set; }
+	[Property]
+	[Category("Components")]
+	public CharacterController Controller { get; set; }
+	[Property]
+	[Category("Components")]
+	public CitizenAnimationHelper Animator { get; set; }
+	
+	/// <summary>
+	/// How fast can you walk (Units per second)
+	/// </summary>
+	[Property]
+	[Category("Stats")]
+	[Range(0f, 400f, 1f)]
+	public float WalkSpeed { get; set; } = 120f;
+	/// <summary>
+	/// How fast can you run (Units per second)
+	/// </summary>
+	[Property]
+	[Category("Stats")]
+	[Range(0f, 800f, 1f)]
+	public float RunSpeed { get; set; } = 250f;
+	/// <summary>
+	/// How powerful can you jump (Units per second)
+	/// </summary>
+	[Property]
+	[Category("Stats")]
+	[Range(0f, 1000f, 10f)]
+	public float JumpStrength { get; set; } = 400f;
+
+	/// <summary>
+	/// Where the camera rotates around and the aim originates from
+	/// </summary>
+	[Property]
+	public Vector3 EyePosition {get; set;}
+	public Vector3 EyeWorldPosition => Transform.Local.PointToWorld(EyePosition);
+	public Angles EyeAngles { get; set; }
+	Transform _initialCameraTransform;
+
+	protected override void DrawGizmos() {
+		Gizmo.Draw.LineSphere( EyePosition, 10f );
+	}
+	
+	protected override void OnUpdate()
+	{
+		EyeAngles += Input.AnalogLook;
+		EyeAngles = EyeAngles.WithPitch( MathX.Clamp( EyeAngles.pitch, -80f, 80f ) );
+		Transform.Rotation = Rotation.FromYaw( EyeAngles.yaw );
+
+		if ( Camera != null)
+		{
+			var cameraTransform = _initialCameraTransform.RotateAround( EyePosition, EyeAngles.WithYaw( 0f ));
+			var cameraPosition = Transform.Local.PointToWorld( cameraTransform.Position );
+			var cameraTrace = Scene.Trace.Ray( EyeWorldPosition, cameraPosition )
+				.Size( 5f )
+				.IgnoreGameObjectHierarchy( GameObject )
+				.WithoutTags( "player" )
+				.Run();
+			
+			Camera.Transform.Position = cameraTrace.EndPosition;
+			Camera.Transform.LocalRotation = cameraTransform.Rotation;
+		}
+	}
+
+	protected override void OnFixedUpdate(){
+		base.OnFixedUpdate();
+
+		if ( Controller == null ) return;
+
+		var wishSpeed = Input.Down("Run") ? RunSpeed : WalkSpeed;
+		var wishVelocity = Input.AnalogMove.Normal * wishSpeed * Transform.Rotation;
+
+		Controller.Accelerate( wishVelocity );
+
+		if ( Controller.IsOnGround ){
+			Controller.Acceleration = 10f;
+			Controller.ApplyFriction( 5f, 20f );
+
+			if( Input.Pressed("Jump") )
+			{
+				Controller.Punch( Vector3.Up * JumpStrength );
+
+				if ( Animator != null)
+				{
+					Animator.TriggerJump();
+				}
+			}
+		}else{
+			Controller.Acceleration = 5f;
+			Controller.Velocity += Scene.PhysicsWorld.Gravity * Time.Delta;
+		}
+		Controller.Move();
+
+		if ( Animator != null ) {
+			Animator.IsGrounded = Controller.IsOnGround;
+			Animator.WithVelocity( Controller.Velocity );
+		}
+	}
+
+	protected override void OnStart(){
+		if ( Camera != null)
+		{
+			_initialCameraTransform = Camera.Transform.Local;
+		}
+
+		if( Components.TryGet<SkinnedModelRenderer> ( out var model ))
+		{
+			var clothing = ClothingContainer.CreateFromLocalUser();
+			clothing.Apply( model );
+		}
+	}
+
+	protected override void OnEnabled(){
+		base.OnEnabled();
+	}
+
+	protected override void OnDisabled(){
+		base.OnDisabled();
+	}
+
+	protected override void OnDestroy() {
+		base.OnDestroy();
+	}
+}
